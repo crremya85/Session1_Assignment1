64d4b443e7fb173c02c69508cb72ee64ea4af0e2
Remya C R <crremya85@gmail.com>
2017-08-04 15:42:01 +0530

    Session1_Assignment1

--- src/Assignment1/AssignmentOne.java
+++ src/Assignment1/AssignmentOne.java
@@ -1,7 +1,14 @@
 package Assignment1;
 
+/**
+ * Class is created for printing Hello World! with the simple basic class and a main method
+ */
+
 public class AssignmentOne {
 
+	/**
+	 * Below is the main method where the String hello World is printed to the console
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 System.out.println("Hello World!!");
